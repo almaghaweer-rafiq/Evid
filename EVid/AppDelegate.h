76273c4b04bfc5401a385715a9fba0e6f95bd16b
ghulamrasool1 <raja@Rajas-MacBook-Pro-2.local>
2017-12-21 13:16:14 +0400

    Initial Commit

---
+++ EVid/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EVid
+//
+//  Created by Raja on 12/21/17.
+//  Copyright © 2017 ghulamrasool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EVid/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EVid
+//
+//  Created by Raja on 12/21/17.
+//  Copyright © 2017 ghulamrasool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
